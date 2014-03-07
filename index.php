e67fbd495e4e3f476058e1f7fa05c3af9f577ccd
alex <markov1991@mail.ru>
2014-03-07 15:49:10 +0700

    init

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/MyLogInSection.css">
+	<link rel="stylesheet" type="text/css" href="./css/MainBody.css">
+	<title>
+		моя маленькая качалка
+	</title>
+</head>
+<body id="MainBody">
+	<section id="MyLogInSection">
+	<table id="MyLogInTable">
+		<tr>
+			<td colspan="2">
+				<input type="text" id="MyLogIn" value="Псевдоним или адрес электронной почты">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="password" id="MyPassword">
+			<td>
+				<button id="MyLogInButton">Войти</button>
+			</td>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<p id="RememberMeChecked"><input type="checkbox" name="1" value="1">Запомнить меня</p>
+			</td> 
+			<td>
+				<button id="MyRememberPassword">Забыли пароль ?</button>
+			</td>
+		</tr>
+	</table>
+	</section>
+</body>
+</html>
